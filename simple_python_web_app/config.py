0a7285639ef232552f0e605f3d207055addb6eed
zzamzam <dangeron@gmail.com>
2023-10-13 14:04:51 +0300

    bump version to 1.2.1

--- simple_python_web_app/config.py
+++ simple_python_web_app/config.py
@@ -1,7 +1,7 @@
 from os import environ
 
 config = {
-    'app_version': "1.0.0",
+    'app_version': "1.2.1",
     'client_id': environ.get("CLIENT_ID", "DefaultID"),
     'client_secret': environ.get("CLIENT_SECRET", "DefaultSecret"),
     'http_port': environ.get("HTTP_PORT", 8080),
