5108b9140af456c16aa122816c0dbf592c9717e4
zzamzam <dangeron@gmail.com>
2023-10-13 14:04:18 +0300

    response with json

--- simple_python_web_app/http_handler.py
+++ simple_python_web_app/http_handler.py
@@ -1,18 +1,19 @@
 from http.server import BaseHTTPRequestHandler
 from config import config
+import json
 
 
 class HttpHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
+    def _set_headers(self):
         self.send_response(200)
-        self.send_header("Content-type", "text/html")
+        self.send_header('Content-type', 'application/json')
         self.end_headers()
-        self.wfile.write('<html><head><meta charset="utf-8">'.encode())
-        self.wfile.write('<title>simple_python_web_app</title></head>'.encode())
-        self.wfile.write(f"""
-                            <body>
-                              <p><b>ID клиента:</b> {config['client_id']}</p>
-                              <p><b>Секрет клиента:</b> {config['client_secret']}</p>
-                            </body>
-                         """.encode())
-        self.wfile.write(f'</html>'.encode())
+
+    def do_GET(self):
+        self._set_headers()
+        response = {
+          'app_version': f'{config["app_version"]}',
+          'client_id': f'{config["client_id"]}',
+          'client_secret': f'{config["client_secret"]}'
+        }
+        self.wfile.write(json.dumps(response).encode(encoding='utf_8'))
